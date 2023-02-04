5cd967c72f3e4833f4e05df2d510dbb113f9b5ed
ictsu631310440 <110732428+takemiethere@users.noreply.github.com>
2023-02-05 00:46:47 +0700

    add Rand Num
    +Random Number to use with Swap function.

--- Assets/Script/RandomActiveButton.cs
+++ Assets/Script/RandomActiveButton.cs
@@ -1,62 +1,105 @@
 using UnityEngine;
+
 using System.Collections;
+using System;
 
 public class RandomActiveButton : MonoBehaviour
 {
-    /*   
-       public GameObject[] buttons;
-
-       void Start()
-       {
-           RandomSwap();
-       }
-
-       public void RandomSwap()
-       {
-           int randomIndex = Random.Range(0, buttons.Length);
-           for (int i = 0; i < buttons.Length; i++)
-           {
-               if (i == randomIndex)
-                   buttons[i].SetActive(true);
-               else
-                   buttons[i].SetActive(false);
-           }
-       }
-
-
-
-       /*public float appearInterval = 10f;
-       public float disappearInterval = 2f;
-
-       void Start()
-       {
-           Invoke("Appear", Random.Range(0, appearInterval));
-       }
-
-       void Appear()
-       {
-           gameObject.SetActive(true);
-           Invoke("Disappear", disappearInterval);
-       }
-
-       void Disappear()
-       {
-           gameObject.SetActive(false);
-           Invoke("Appear", Random.Range(0, appearInterval));
-
-   */
-
-
-
-
-    /*public GameObject button1;
-    public GameObject button2;
+    public GameObject normalBT , SwapBT;
+    public float normalForm, swapForm;
+    public int numToSwap, numToTurnBack;
 
     void Start()
     {
-        int randomIndex = Random.Range(0, 2);
-        button1.SetActive(randomIndex == 0);
-        button2.SetActive(randomIndex == 1);*/
+
+        numToSwap = UnityEngine.Random.Range(10, 20);
+        Debug.Log(numToSwap);
+
+        numToTurnBack = UnityEngine.Random.Range(5, 7);
+        Debug.Log(numToTurnBack);
+
+
+        normalBT.SetActive(true);
+        SwapBT.SetActive(false);
+        Invoke("SwitchButtons", normalForm = numToSwap);
+    }
+
+    void SwitchButtons()
+    {
+        normalBT.SetActive(false);
+        SwapBT.SetActive(true);
+        Invoke("TurnBack", swapForm = numToTurnBack);
+    }
+
+    void TurnBack()
+    {
+        normalBT.SetActive(true);
+        SwapBT.SetActive(false);
+    }   
+}
+    
+
+
+
+
+
+
+
+
+
+/*public GameObject[] buttons;
+
+    void Start()
+    {
+        RandomSwap();
+    }
+
+    public void RandomSwap()
+    {
+        int randomIndex = Random.Range(0, buttons.Length);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i == randomIndex)
+                buttons[i].SetActive(true);
+            else
+                buttons[i].SetActive(false);
+        }
+    }
+
+
+
+    *//*public float appearInterval = 10f;
+    public float disappearInterval = 2f;
+
+    void Start()
+    {
+        Invoke("Appear", Random.Range(0, appearInterval));
+    }
+
+    void Appear()
+    {
+        gameObject.SetActive(true);
+        Invoke("Disappear", disappearInterval);
+    }
+
+    void Disappear()
+    {
+        gameObject.SetActive(false);
+        Invoke("Appear", Random.Range(0, appearInterval));
+
+
+
+
+
+
+/*public GameObject button1;
+public GameObject button2;
+
+void Start()
+{
+ int randomIndex = Random.Range(0, 2);
+ button1.SetActive(randomIndex == 0);
+ button2.SetActive(randomIndex == 1);*/
 
 
     /*public float appearInterval = 10f; // ?????????
@@ -85,30 +128,8 @@ public class RandomActiveButton : MonoBehaviour
         button2.SetActive(false);
     }*/
 
-
-    public GameObject button1;
-    public GameObject button2;
-    public float appearInterval = 2f;
-    public float disappearInterval = 2f;
-
-    void Start()
+    /*void Start()
     {
-        button1.SetActive(true);
-        button2.SetActive(false);
-        Invoke("SwitchButtons", appearInterval);
-    }
-
-    void SwitchButtons()
-    {
-        button1.SetActive(false);
-        button2.SetActive(true);
-        Invoke("TurnBack", disappearInterval);
-    }
-
-    void TurnBack()
-    {
-        button1.SetActive(true);
-        button2.SetActive(false);
-    }
-}
-
+        int randomNumber = UnityEngine.Random.Range(1, 101);
+        Debug.Log(randomNumber);
+    }*/
