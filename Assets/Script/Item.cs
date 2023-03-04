877dbe3a1d110c8b3dd31afcc7cc7522f200f115
ict631310446 <phumpuang_t@silpakorn.edu>
2023-03-04 23:17:13 +0700

    add Random Speed item

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -4,42 +4,45 @@ public class Item : MonoBehaviour
 {
     public ModeManager modeManager;
     public RandomMode randomMode;
-    public bool SetMode;
+    public bool setMode;
     public int newModeIndex;
 
-
-    public float speed = 3f; // Speed at which the object moves
+    public float minSpeed = 1f; // Minimum speed at which the object moves
+    public float maxSpeed = 5f; // Maximum speed at which the object moves
     public float loopDistance = 5f; // Distance the object moves before looping
 
     private float startPosition; // Starting position of the object
     private bool movingLeft = true; // Whether the object is currently moving left or right
+    private float currentSpeed; // Current speed of the object
 
     void Start()
     {
         startPosition = transform.position.x; // Get the starting position of the object
+        currentSpeed = Random.Range(minSpeed, maxSpeed); // Set the initial speed of the object to a random value
     }
-
 
     void Update()
     {
         float direction = movingLeft ? -1f : 1f; // Determine the direction the object should move
 
-        transform.Translate(Vector3.right * direction * speed * Time.deltaTime); // Move the object
+        transform.Translate(Vector3.right * direction * currentSpeed * Time.deltaTime); // Move the object using the current speed
 
-        // If the object has moved the loop distance, reverse its direction
+        // If the object has moved the loop distance, reverse its direction and set a new random speed
         if (movingLeft && transform.position.x < startPosition - loopDistance)
         {
             movingLeft = false;
+            currentSpeed = Random.Range(minSpeed, maxSpeed);
         }
         else if (!movingLeft && transform.position.x > startPosition + loopDistance)
         {
             movingLeft = true;
+            currentSpeed = Random.Range(minSpeed, maxSpeed);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        item_Set(SetMode);
+        item_Set(setMode);
         Destroy(gameObject);
     }
 
---
+++ Assets/Script/SceneChanger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        //Reset Time
+        Time.timeScale = 1;
+
+        SceneManager.LoadScene(sceneName);
+    }
+}
