5443b44c737b7307765cf5068b52ec563b3cdbac
ict631310446 <phumpuang_t@silpakorn.edu>
2023-03-05 12:28:04 +0700

    update Double cant repeated 3 time
    + get item 1,2  Isplay -> random 1,2,3
    + get item 3 Isplay -> swap

--- Assets/Script/RandomMode.cs
+++ Assets/Script/RandomMode.cs
@@ -3,10 +3,24 @@ using UnityEngine;
 public class RandomMode : MonoBehaviour
 {
     public ModeManager modeManager;
+    public int IsPlayed = 1;
+    public int newModeIndex;
 
     public void ChangeModeRandomly()
     {
-        int newModeIndex = Random.Range(1, modeManager.gameModeList.Length);
-        modeManager.SetMode(newModeIndex);
+        newModeIndex = Random.Range(1, modeManager.gameModeList.Length);
+        if (IsPlayed < 3)
+        {
+            if (newModeIndex is 2 or 3)
+            {
+                IsPlayed++;
+                modeManager.SetMode(newModeIndex);
+            }
+        }
+        else
+        {
+            modeManager.SetMode(1);
+            /*ChangeModeRandomly();*/
+        }
     }
 }
